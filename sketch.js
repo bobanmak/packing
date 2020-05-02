44a108959f7e17b65da9f0bda2cce4c54a2bc191
Boban Jordanoski <boban.jordanoski@iem.thm.de>
2020-05-02 19:43:00 +0200

    added lines

--- sketch.js
+++ sketch.js
@@ -13,16 +13,16 @@ function setup() {
     createCanvas(720, 400);
     background(0);
     noSmooth();
-  
-    translate(140, 0);
-  
-    // Draw gray box
     stroke(153);
+    line(...walls[0]);  // Draw Line
+    line(...walls[1]);  // Draw Line
+    line(...walls[2]);  // Draw Line
+    line(...walls[3]);  // Draw Line
   
     let wallsSize = Object.keys(walls).length;
     let last, next;
-    for( let i = 0; i < wallsSize; i++ ){
-        line(...walls[i]);  // Draw Line
+    for( let i = 0; i < 4; i++ ){
+        //line(...walls[i]);  // Draw Line
         last = i === 0              ? walls[ wallsSize - 1 ] : walls[i-1]; 
         next = i === wallsSize - 1  ? walls[0]               : walls[i+1]; 
         let n = newOuterPoint( walls[i], last, next);
@@ -39,13 +39,20 @@ function setup() {
     let v1 = createVector( pNext[0] , pNext[1] );
     let v2 = createVector( pLast[0] , pLast[1] );
 
-    //draw( [v0,v1,v2]);
+    stroke(255,0,0); //rot
+    line( p[0]      , pNext[0]+100  , p[1]   ,pNext[1]-100);
+    line( pLast[0]  , p[2]+100, pLast[0] ,p[3]-100);
 
-    drawArrow( v0, v1, 'red' );
-    drawArrow( v0, v2, 'blue' );
-  
-    console.log( v0, v1, v2 );
- 
+
+    stroke(0,0,255); //blau
+    line( pLast[0]-100 , p[1], pLast[1]+100 ,p[1]);
+    line( p[2]+100 , pNext[1], p[3]-100 ,pNext[1]);
+
+
+
+    //drawArrow( v0, v1, 'red' );
+    //drawArrow( v0, v2, 'blue' );
+   
     let tetha1 = v1.angleBetween(v2);
 
     console.log("actuel: ", p, "before: ", pLast, degrees(tetha1), "next: ", pNext);
